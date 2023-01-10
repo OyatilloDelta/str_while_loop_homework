bb34d1e273aabfb175ef9f67ee90628dd490720e
unknown <araves2003@gmail.com>
2023-01-10 05:19:28 -0800

    07

--- While07.py
+++ While07.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    j=0
+    while i<len(s):
+        if int(s[i])%(2)==0:
+            j+=1
+        i+=1
+    return j
+print(main('23456777'))
